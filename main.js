e855383f8a4202de1cdaefb587105dc5d94dad7d
hyesa <hyesa.araujo@escola.pr.gov.br>
2025-06-13 10:34:48 -0300

    Jogo da Cobra

---
+++ main.js
@@ -0,0 +1,38 @@
+let cobra = [{x:10, y:10}];
+let comida = gerarComida();
+let direcao = 'DIREITA';
+let velocidade = 150;
+let pontos = 0;
+let intervalo;
+
+function iniciar(){
+    criarTabuleiro();
+    document.addEventListener('keydown', mudarDirecao);
+    intervalo = setInterval(moverCobra, velocidade);
+}
+
+function criarTabuleiro(){
+    const tabuleiro = document.getElementById('game-board');
+    tabuleiro.innerHTML = '';
+
+    cobra.forEach(segmento =>  {
+        const elemento = document.createElement('div');
+        elemento.style,gridRowStart = segmento.y;
+        elemento.style.gridColumnStart = segmento.x;
+        elemento.classList.add('snake');
+        tabuleiro.appendChild(elemento);
+    } );
+    function gerarComida(){
+        let novaComida;
+        while (!novaComida || posicaoOcupada(novaComida)){
+        }
+         novaComida = {
+            x:Math.floor(Math.random()*18)+2,
+            y:Math.floor(Math.random()*18)+2
+         
+         };
+
+        }
+    }
+
+
